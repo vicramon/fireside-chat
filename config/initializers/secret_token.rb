c3c096297a0da63ebf10bf8ebfa1906b7535354c
Vic Ramon <vic@vicramon.com>
2014-01-28 10:40:29 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FiresideChat::Application.config.secret_key_base = 'e633a1b4884203cab1517734f49fbd8ea5d663a3870a39dc37594a988ee94bd7d0fe7f57873177c45d575d60c386537185bb2276b3259df2d56489facad02902'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FiresideChat::Application.config.session_store :cookie_store, key: '_fireside-chat_session'
